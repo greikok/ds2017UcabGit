8486646cecd7cf6f4ebbff4ed2748f2f74ed45f6
Atahualpa Silva F. <atahualpasilva@gmail.com>
2017-06-23 20:03:52 -0400

    Creando servicio web de reportes.

---
+++ DoctorWebServiciosWCF/Services/IReporteService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DoctorWebServiciosWCF.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IReporteService" in both code and config file together.
+    [ServiceContract]
+    public interface IReporteService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/obtener/{codigo}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string DoWork(string codigo);
+    }
+}
