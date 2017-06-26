93613283329115f7de65279e7edabf2512cdfdfe
Roysbert Salinas <rasc.19@gmail.com>
2017-06-26 19:21:23 -0400

    Ajustes en Cliente y Servicios

---
+++ DoctorWebServiciosWCF/Services/IReportesService.cs
@@ -0,0 +1,20 @@
+﻿using DoctorWebServiciosWCF.Models.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DoctorWebServiciosWCF.Services
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IReportesService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IReportesService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/Ejemplo?parametro={codigo}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        ResultadoProceso DoWork(string codigo);
+    }
+}
---
+++ DoctorWebServiciosWCF/Services/ReportesService.svc.cs
@@ -0,0 +1,30 @@
+﻿using DoctorWebServiciosWCF.Models.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DoctorWebServiciosWCF.Services
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ReportesService" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ReportesService.svc o ReportesService.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ReportesService : IReportesService
+    {
+        public ResultadoProceso DoWork(string codigo)
+        {
+            var resultado = new ResultadoProceso();
+            try
+            {
+                resultado.Mensaje = "Bienvido!";
+                resultado.SinProblemas = true;
+            }
+            catch (Exception ex)
+            {
+                resultado.Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+    }
+}
